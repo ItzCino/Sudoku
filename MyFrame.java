7684f270c5360af066394f9f5908cb4560b68211
Zhi Feng Chen <itzcino@gmail.com>
2023-04-19 02:28:21 +1200

    working JTextfield grid

--- MyFrame.java
+++ MyFrame.java
@@ -3,15 +3,14 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-import javax.swing.text.Position;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -25,73 +24,129 @@ public class MyFrame extends JFrame implements ActionListener, DocumentListener{
   int noOfTextFields = boxSize* boxSize*boxSize;
   int noOfRows = boxSize*boxSize;
   int noOfColumns = boxSize* boxSize;
-  JPanel totalGUI = new JPanel();
+  // creates master panel
+  JPanel mainPanel = new JPanel(new GridLayout(boxSize, boxSize, 1, 1));
+  // creates a 3x3 cluster of pannels
+  JPanel[][] panels = new JPanel[boxSize][boxSize];
+  int[][] sudokuData = new int[noOfRows][noOfColumns];
 
-  MyFrame() {
+  public MyFrame() {
     this.setTitle("Susdoku");  // sets title of window
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // exits program when window is closed
     this.setResizable(false);  // prevents window from being resized
-    this.setSize(750, 650);
+    this.setSize(400, 400);
+    this.setLocation(300, 300);
+
+    mainPanel.setLocation(100, 250);;
+    
+
+    for (int row = 0; row < boxSize; row++) {
+      for (int col = 0; col < boxSize; col++) {
+        JPanel panel = new JPanel(new GridLayout(boxSize, boxSize, 1, 1));
+        panel.setBackground(Color.GRAY);
+        panel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
+        for (int i = 0; i < boxSize; i++) {
+          for (int j = 0; j < boxSize; j++) {
+            JPanel panelTemplate = new JPanel(new GridLayout(boxSize, boxSize, 1, 1));
+            panelTemplate.setBackground(Color.GRAY);
+            panelTemplate.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
+            JTextField text = newTextField();
+            text.getDocument().addDocumentListener(this);
+            panelTemplate.add(text);
+            panel.add(panelTemplate);
+            
+          }
+        }
+        mainPanel.add(panel);
+        // JTextField text = newTextField();
+        // text.getDocument().addDocumentListener(this);
+      }
+    }
+
+    // for (int row = 0; row < panels.length; row++) {
+    //   for (int col = 0; col < panels[row].length; col++) {
+    //     JPanel panel = new JPanel(new GridLayout(boxSize, boxSize, 1, 1));
+    //     panel.setBackground(Color.GRAY);
+    //     panel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
+    //     panels[row][col] = panel;
+    //     // JTextField text = newTextField();
+    //     // text.getDocument().addDocumentListener(this);
+    //     // panel.add(text);
+    //     // mainPanel.add(panel);
+    //   }
+    // }
+    // for (int i=0;i<panels.length;i++) {
+    //   for (int j=0;j<panels[i].length;j++) {
+    //     mainPanel.add(panels[i][j]);
+    //   }
+    // }
+
+    // createClusters();
 
     // this.setLayout(new FlowLayout());  // sets layout manager
 
     // sets background color of window & sets an icon image
-    this.getContentPane().setBackground(new Color(1, 140, 140));
+    this.getContentPane().setBackground(new Color(255, 255, 255));
     ImageIcon windowIcon = new ImageIcon("sus.png");
     this.setIconImage(windowIcon.getImage());
 
     // button = new JButton("press_me");
     // button.addActionListener(this);
 
-    createFields(noOfRows, noOfColumns);
+    // createGrid(noOfRows, noOfColumns);
+    this.add(mainPanel);
     
     // this.add(button);
     this.setVisible(true); // makes window visible
   }
 
-  public void createFields(int rows, int columns) {
+  public void createClusters() {
+    for (int row = 0; row < noOfRows; row++) {
+      for (int col = 0; col < boxSize; col++) {
+        JPanel panel = new JPanel(new GridLayout(boxSize, boxSize, 1, 1));
+        panel.setBackground(Color.BLACK);
+        panel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
+        mainPanel.add(panel);
+      }
+    }
+  }
+
+  public void createGrid(int rows, int columns) {
     int posX = 50;
     int posY = 100;
-
+    
     for (int r=0; r<rows; r++) {
       JPanel panel = new JPanel();
       panel.setLocation(posX, posY);
       panel.setSize(450, 50);
+
       for (int i=0; i<columns; i++) {
-        JTextField text = newTextField(i);
+        JTextField text = newTextField();
         text.getDocument().addDocumentListener(this);
         textFields.add(text);
         panel.add(text);
-      //   System.out.println(i);
       }
-      totalGUI.add(panel);
-      posY += 50;
-    }
-    this.add(totalGUI);
-    // System.out.println(textFields.size());
-    System.out.println("+");
-    for (int i=0;i<textFields.size();i++) {
-      System.out.println(i);
-    }
-    System.out.println("+");
 
+      posY += 50;
+      mainPanel.add(panel);
+    }
   }
-  public JTextField newTextField(int i) {
+  public JTextField newTextField() {
     JTextField text;
-  // initializes text field
+    // initializes text field
     text = new JTextField();
-    text.setPreferredSize(new Dimension(40, 40));
+    text.setPreferredSize(new Dimension(40, 120));
     text.setBorder(BorderFactory.createLineBorder(Color.black));
-    text.setFont(new Font("Consolas", Font.PLAIN, 40));
+    text.setFont(new Font("Consolas", Font.PLAIN, 20));
     return text;
   }
 
   // displays stored text
   public void printInput() {
-    System.out.println("============");
-    for (int i=0;i<noOfRows;i++) {
-      System.out.println(i + " : " + textFields.get(i).getLocation(getLocation()) + " : " +textFields.get(i).getText());
-    }
+    // System.out.println("============");
+    // for (int i=0;i<noOfRows;i++) {
+    //   System.out.println(i + " : " + textFields.get(i).getLocation(getLocation()) + " : " +textFields.get(i).getText());
+    // }
   }
 
   @Override
