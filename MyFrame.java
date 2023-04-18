34a5ed8c31203833fa7b098a66a0b7af606a2903
Zhi Feng Chen <itzcino@gmail.com>
2023-04-19 03:18:50 +1200

    centred text and editable fields

--- MyFrame.java
+++ MyFrame.java
@@ -34,54 +34,31 @@ public class MyFrame extends JFrame implements ActionListener, DocumentListener{
     this.setTitle("Susdoku");  // sets title of window
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // exits program when window is closed
     this.setResizable(false);  // prevents window from being resized
-    this.setSize(400, 400);
+    this.setSize(750, 750);
     this.setLocation(300, 300);
 
-    mainPanel.setLocation(100, 250);;
+    // mainPanel.setLocation(100, 250);
     
 
     for (int row = 0; row < boxSize; row++) {
       for (int col = 0; col < boxSize; col++) {
         JPanel panel = new JPanel(new GridLayout(boxSize, boxSize, 1, 1));
-        panel.setBackground(Color.GRAY);
-        panel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
+        // JPanel panel = new JPanel(new BorderLayout());
+        panel.setBackground(Color.BLACK);
+        panel.setBorder(BorderFactory.createEmptyBorder(1, 1, 1, 1));
         for (int i = 0; i < boxSize; i++) {
           for (int j = 0; j < boxSize; j++) {
-            JPanel panelTemplate = new JPanel(new GridLayout(boxSize, boxSize, 1, 1));
-            panelTemplate.setBackground(Color.GRAY);
-            panelTemplate.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
-            JTextField text = newTextField();
+            JPanel panelTemplate = new JPanel();
+            panelTemplate.setBackground(Color.BLACK);
+            JTextField text = createTextField();
             text.getDocument().addDocumentListener(this);
             panelTemplate.add(text);
             panel.add(panelTemplate);
-            
           }
         }
         mainPanel.add(panel);
-        // JTextField text = newTextField();
-        // text.getDocument().addDocumentListener(this);
       }
     }
-
-    // for (int row = 0; row < panels.length; row++) {
-    //   for (int col = 0; col < panels[row].length; col++) {
-    //     JPanel panel = new JPanel(new GridLayout(boxSize, boxSize, 1, 1));
-    //     panel.setBackground(Color.GRAY);
-    //     panel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
-    //     panels[row][col] = panel;
-    //     // JTextField text = newTextField();
-    //     // text.getDocument().addDocumentListener(this);
-    //     // panel.add(text);
-    //     // mainPanel.add(panel);
-    //   }
-    // }
-    // for (int i=0;i<panels.length;i++) {
-    //   for (int j=0;j<panels[i].length;j++) {
-    //     mainPanel.add(panels[i][j]);
-    //   }
-    // }
-
-    // createClusters();
 
     // this.setLayout(new FlowLayout());  // sets layout manager
 
@@ -100,47 +77,16 @@ public class MyFrame extends JFrame implements ActionListener, DocumentListener{
     this.setVisible(true); // makes window visible
   }
 
-  public void createClusters() {
-    for (int row = 0; row < noOfRows; row++) {
-      for (int col = 0; col < boxSize; col++) {
-        JPanel panel = new JPanel(new GridLayout(boxSize, boxSize, 1, 1));
-        panel.setBackground(Color.BLACK);
-        panel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
-        mainPanel.add(panel);
-      }
-    }
-  }
-
-  public void createGrid(int rows, int columns) {
-    int posX = 50;
-    int posY = 100;
-    
-    for (int r=0; r<rows; r++) {
-      JPanel panel = new JPanel();
-      panel.setLocation(posX, posY);
-      panel.setSize(450, 50);
-
-      for (int i=0; i<columns; i++) {
-        JTextField text = newTextField();
-        text.getDocument().addDocumentListener(this);
-        textFields.add(text);
-        panel.add(text);
-      }
-
-      posY += 50;
-      mainPanel.add(panel);
-    }
-  }
-  public JTextField newTextField() {
-    JTextField text;
-    // initializes text field
-    text = new JTextField();
-    text.setPreferredSize(new Dimension(40, 120));
-    text.setBorder(BorderFactory.createLineBorder(Color.black));
-    text.setFont(new Font("Consolas", Font.PLAIN, 20));
+  public JTextField createTextField() {
+    JTextField text = new JTextField();
+    text.setPreferredSize(new Dimension(80, 80));
+    text.setFont(new Font("Arial", Font.PLAIN, 30));
+    text.setHorizontalAlignment(JTextField.CENTER);
+    text.setAlignmentY(JTextField.CENTER);
+    text.setBorder(BorderFactory.createLineBorder(Color.BLACK));
     return text;
   }
-
+  
   // displays stored text
   public void printInput() {
     // System.out.println("============");
